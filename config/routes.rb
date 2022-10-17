6167201985aad154c20c9751a8434e48b1ff69e0
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-17 17:12:17 -0600

    Display the first data in the browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'greetings/index'
+      get 'greetings', to: 'greetings#index', defaults: { format: :json }
     end
   end
   root 'root#index'
